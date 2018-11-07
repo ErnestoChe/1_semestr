d11d9a26009025f2ecfbdb8a7268f3e68ca8b282
ErnestoChe <ernestfilippov@mail.ru>
2018-11-07 16:26:01 +0300

    dorabotat' nado

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,7 +2,6 @@ package com.company;
 
 import java.util.LinkedList;
 import java.util.Scanner;
-import java.util.Stack;
 
 public class Main {
 
@@ -16,46 +15,52 @@ class Turing{
     public static void main(String[] args) {
         Scanner sk = new Scanner(System.in);
         int length = sk.nextInt();//Ввод длины ленты
-        LinkedList<Integer> lenta = new LinkedList<Integer>();
+        Scanner sq = new Scanner(System.in);
+        String sq1 = sq.nextLine();
+        char[]b = sq1.toCharArray();
+        LinkedList<Character> lenta = new LinkedList<Character>();
         for (int i = 0; i < length; i++) {
-            int b = sk.nextInt();
-            lenta.add(b);
+            lenta.add(b[i]);
         }
-        System.out.println(lenta);
         Commands cmd = new Commands(length);
-        /*for(int i=0;i<2;i++) {
-            for(int j=0;j<length;j++) {
-                System.out.println(i+" "+j+" "+cmd.commands[i][j]);
-            }
-        }*/
         TuringMachine object=new TuringMachine();
-        System.out.println(object.some_function(lenta, cmd, length));
+        System.out.println(object.changeInLine(lenta, cmd, length));
 
     }
 }
 class TuringMachine{
 
-    public static LinkedList<Integer> some_function (LinkedList<Integer> lenta,Commands com,int length){
-        for(int i=0;i<2;i++) {
-            for(int j=0;j<length;j++) {
-                if(com.commands[i][j]=="R,S1") {
+    public static LinkedList<Character> changeInLine(LinkedList<Character> lenta, Commands com, int length) {
+        Scanner sk = new Scanner(System.in);
+        System.out.println("Введите символ, который хотите поменять");
+        char b = sk.nextLine().charAt(0);       //которое хотим менять
+        System.out.println("Введите символ, на который хотите поменять");
+        char c = sk.nextLine().charAt(0);       //которое меняем
+        for (int i = 0; i < 2; i++) {   //проход слева направо, меняя а на б
+            for (int j = 0; j < length; j++) {
+                if (com.commands[i][j] == "R,S1") { //читаем команду
                     System.out.println(lenta);
                     polz(j);
-                    if(lenta.get(j)==0) {
-                        lenta.remove(j);
-                        lenta.add(j, 1);
+                    if (lenta.get(j) == b) {
+                        lenta.remove(j);    //вставляем на место старой цифры новую
+                        lenta.add(j, c);
+                        System.out.println(lenta);  //логирование ленты
+                        polz(j);                    //ползунок для обозначения
                     }
-                    System.out.println(lenta);
-                    polz(j);
-                }else if(com.commands[i][j]=="L,S2") {
-                    System.out.println(lenta);
-                    polz(j);
-                    if(lenta.get(j)==2) {
-                        lenta.remove(j);
-                        lenta.add(j, 3);
+                }
+            }
+        }
+        for (int k =2-1; 0<=k; k--) {
+            for (int a = length-1; a>=0; a--) {     //проход справа налево, меняя a на 0
+                if (com.commands[k][a] == "L,S2") {
+                    System.out.println(lenta);      //логирование ленты
+                    polz(a);                        //ползунок для обозначения
+                    if (lenta.get(a) == 'a') {      //считываем "а"
+                        lenta.remove(a);            //вставляем на место а нуль
+                        lenta.add(a, '0');
                     }
-                    System.out.println(lenta);
-                    polz(j);
+                    System.out.println(lenta);      //логирование ленты
+                    polz(a);                        //ползунок для обозначения
                 }
             }
         }
@@ -68,7 +73,6 @@ class TuringMachine{
         }
         System.out.println(p);
     }
-
 }
 class Commands{
     String [][] commands;
@@ -76,12 +80,9 @@ class Commands{
         this.commands=new String[2][lenta];
         for(int j = 0; j<lenta;j++) {
             this.commands[0][j] ="R,S1";
-
         }
         for(int i =0; i<lenta; i++) {
             this.commands[1][i] = "L,S2";
         }
-
     }
-
 }
